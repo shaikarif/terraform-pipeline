51e8eaf185f6792ba54dab0b41c7f29a216f7bbd
shaikarif <ec2-user@ip-172-31-18-45.us-west-2.compute.internal>
2021-07-23 13:23:50 +0000

    added variables in main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region = "us-west-2"
-  Access_key_ID = "${var.Access_key_ID}"
-  Secret_access_key = "${var.Secret_access_key}"
+  Access_key_ID = "AKIA5UNHNPBAM6ZIBOFK"
+  Secret_access_key = "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"
 }
 
 resource "aws_instance" "JenkinsMaster" {
