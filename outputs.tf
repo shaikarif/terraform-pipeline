83848308e6ff33ffe3168ad383a213f28d356589
shaikarif <ec2-user@ip-172-31-18-45.us-west-2.compute.internal>
2021-07-23 17:48:36 +0000

    change in output file

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,21 @@ output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.JenkinsMaster.public_ip
 }
+
+
+output "instance_type" {
+
+     value = aws_instance.JenkinsMaster.instance_type
+}
+
+output "instance_state" {
+       value = aws_instance.JenkinsMaster.instance_state
+}
+
+output "public_dns" {
+          value = aws_instance.JenkinsMaster.public_dns
+}
+
+
+
+
