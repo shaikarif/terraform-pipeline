005cc684f55ba935f7a64942615f28cb582c01af
shaikarif <ec2-user@ip-172-31-18-45.us-west-2.compute.internal>
2021-07-23 15:57:49 +0000

    changes made in main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,9 @@ resource "aws_instance" "JenkinsMaster" {
   ami = "ami-0dc8f589abe99f538"
   instance_type = "t2.micro"
   availability_zone = "us-west-2b"
+  tags =  { 
+    Name= "Testing_instance"
+}
 
 }
 
