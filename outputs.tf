e152afaed761f887374112e615e15aedfa2fb30a
shaikarif <ec2-user@ip-172-31-18-45.us-west-2.compute.internal>
2021-07-23 16:53:06 +0000

    Changes made in outputs file

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,11 @@
 output "instance_id"  {
 
-   value = aws_instance.id
+   value = aws_instance.app_server.id
 
+
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
 }
