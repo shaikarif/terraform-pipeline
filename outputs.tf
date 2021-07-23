fbffbad6b9d1fe2d7a5e5ec6345104c86520525d
shaikarif <ec2-user@ip-172-31-18-45.us-west-2.compute.internal>
2021-07-23 17:00:03 +0000

    Changes made in outputs file

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,5 @@
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.jenkins_server.public_ip
+  value       = aws_instance.jenkins_instance.public_ip
 }
