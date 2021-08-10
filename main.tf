7b42bccfcbbd03f1db27676d351e93af7f324683
shaikarif <shaikarifshaiks@gmail.com>
2021-08-10 15:57:02 +0000

    Added all the files

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.51.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+  shared_credentials_file = "/home/ec2-user/.aws/credentials"
+}
+
+
+resource "aws_vpc" "Main"  {
+     cidr_block  = "10.0.0.0/16"
+}
+resource "aws_internet_gateway" "Main" {
+       vpc_id = aws_vpc.Main.id
+       tags = {
+           Name = "terraform-igw"
+    }
+
+}
+resource "aws_instance" "JenkinsMaster" {
+  ami = "ami-0dc8f589abe99f538"
+  instance_type = "t2.micro"
+  availability_zone = "us-west-2b"
+  tags =  { 
+    Name= "jenkins_instance"
+}
+
+}
+resource "aws_s3" "My_bucket" {
+       acl = "private"
+       tags = {
+       Name = "My_Bucket"
+       }
+  }
+
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "Acces_key_ID" {
+     type = string
+     default = ""
+}
+
+variable "Secret_access_key" {
+       type = string 
+       default = ""
+ }
+
